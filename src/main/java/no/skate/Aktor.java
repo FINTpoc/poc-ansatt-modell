4d4b7ae1e80008484e31b5baa0480f4680c8de2a
jarlehansen <jarle@jarlehansen.net>
2016-04-18 21:16:57 +0200

    initialize identifikator-list to an empty list and include an add-method

--- src/main/java/no/skate/Aktor.java
+++ src/main/java/no/skate/Aktor.java
@@ -3,16 +3,22 @@ package no.skate;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
 import java.util.List;
 
 @XmlType(name = "Aktor")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Aktor {
-    private List<Identifikator> identifikatorer;
+    private List<Identifikator> identifikatorer = new ArrayList<>();
     private Kontaktinformasjon kontaktinformasjon;
+
 
     public List<Identifikator> getIdentifikatorer() {
         return identifikatorer;
+    }
+
+    public void addIdentifikator(Identifikator identifikator) {
+        identifikatorer.add(identifikator);
     }
 
     public void setIdentifikatorer(List<Identifikator> identifikatorer) {
